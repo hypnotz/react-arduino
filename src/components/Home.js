5134476cd45c1ccb3cca3cf83938699ee0d62cf3
hypnotz <iv.soliso@gmail.com>
2022-02-06 20:22:57 -0300

    Header y footer material ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Lista from "./Lista";
-
+import MenuList from "./MenuList";
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/Home.css"
 
 const Home = () => {
   return (
-    <Container>
-      <Lista />
-    </Container>
+    <>
+      <Header />
+      <Container className="content">
+        <MenuList />
+        <Lista />
+      </Container>
+      <Footer />
+    </>
   );
 };
 
