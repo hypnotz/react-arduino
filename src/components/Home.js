9693df9d8c0b82213e0531713e6e24fcca5d56f9
hypnotz <iv.soliso@gmail.com>
2022-02-06 19:57:39 -0300

    Lista Material UI

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Lista from "./Lista";
+
+
+const Home = () => {
+  return (
+    <Container>
+      <Lista />
+    </Container>
+  );
+};
+
+export default Home;
