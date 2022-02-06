18543efeb1ddea83691372926e88d8b292aed291
hypnotz <iv.soliso@gmail.com>
2022-02-06 19:57:54 -0300

    List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
-    <Login />
+    {/* <Login /> */}
+    <Home />
     </>
   );
 }
